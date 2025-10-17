2d783ecd05960c84a328420b2e621a7b78f31fe6
dsgallups <dsgallups@protonmail.com>
2025-10-17 05:00:13 -0400

    idk

--- src/brain.rs
+++ src/brain.rs
@@ -1,1 +1,16 @@
+use crate::prelude::NumJunction;
+
 pub struct Brain {}
+
+impl Brain {
+    // To "bring in"
+    pub fn affer(&self, input: &NumJunction) {}
+
+    pub fn effer(&self, output: &NumJunction) {}
+}
+
+impl Default for Brain {
+    fn default() -> Self {
+        Self {}
+    }
+}
